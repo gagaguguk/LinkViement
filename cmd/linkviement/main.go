71ba0d2ef9b2782843697651e61294dbd0369b57
Gaga <116130695+gagaguguk@users.noreply.github.com>
2026-02-02 16:40:14 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/linkviement/main.go
@@ -0,0 +1,23 @@
+// cmd/linkviement/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "linkviement/internal/linkviement"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := linkviement.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
